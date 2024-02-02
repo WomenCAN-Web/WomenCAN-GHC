f2a2e3c07fa19b4675dd63a19ebdd227e09f907b
Siddhartha Reddy <siddharsha675@gmail.com>
2024-02-01 20:49:36 -0600

    add _app.js and setting up

---
+++ my-next-app/src/app/_app.js
@@ -0,0 +1,9 @@
+// src/_app.js or src/_app.tsx
+
+import 'C:\Users\sandhya reddy\OneDrive\Desktop\womenCAN\WomenCAN-GHC-1\my-next-app\src\app\globals.css'; // Adjust the path if your globals.css is located elsewhere
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
